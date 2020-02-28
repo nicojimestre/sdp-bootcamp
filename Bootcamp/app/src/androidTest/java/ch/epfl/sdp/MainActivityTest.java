5ac2eb1f4cd9f92d48059743776415fe3d100f42
nicojimestre <njimenez5007@gmail.com>
2020-02-28 02:06:48 +0100

    final commit on other branch

--- Bootcamp/app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
+++ Bootcamp/app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -14,7 +14,9 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(value = AndroidJUnit4.class)
 public class MainActivityTest {
@@ -25,6 +27,8 @@ public class MainActivityTest {
     public void testCanGreetUsers() {
         onView(withId(R.id.mainText)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
         onView(withId(R.id.MainButton)).perform(click());
-        // onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
+        onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
     }
+
+
 }
--- Bootcamp/app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ Bootcamp/app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -2,7 +2,10 @@ package ch.epfl.sdp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, GreetingActivity.class);
+        EditText editText = (EditText) findViewById(R.id.mainText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+
 }
